34126de17bc215e6129321c2b2eb1ab01ae76ccb
Azay Genius <azaygenius@Kekelis-MacBook-Air.local>
2022-03-26 13:16:45 +0000

    initial screens created

---
+++ components/Home.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import Listen from "./BottomTab/ListenNow"; 
+import Browse from './BottomTab/Browse';
+import Library from './BottomTab/Library';
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: 'gray'
+
+    }
+})
