22592aa3cf4bc3e909a56ad41a528d7edab09534
Azay Genius <azaygenius@Kekelis-MacBook-Air.local>
2022-04-02 18:21:25 +0000

    New component modal created

--- components/Account.js
+++ components/Account.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function Account() {
+export default function Account({navigation}) {
   return (
     <View>
       <Text>Account</Text>
