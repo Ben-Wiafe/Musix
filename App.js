c8571fcfce14006692a0599c3217b5f51e1e757a
Azay Genius <azaygenius@Kekelis-MacBook-Air.local>
2022-04-02 18:21:02 +0000

    Navigation setup

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, Modal } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Account from "./components/Account";
+import Setting from "./components/Setting";
 import Icon from "react-native-vector-icons/Ionicons";
+import IconModal from "./components/IconModal";
 import Home from "./components/Home";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -19,6 +23,8 @@ export default function App() {
         }}
       >
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Account" component={Account} />
+        <Stack.Screen name="Setting" component={Setting} />
       </Stack.Navigator>
     </NavigationContainer>
   );
